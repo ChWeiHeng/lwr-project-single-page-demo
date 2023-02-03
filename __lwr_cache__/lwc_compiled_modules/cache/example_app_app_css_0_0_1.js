1adeccd5626815227422cccfead0fd6fc9e031ee
chenweiheng <1164323899@qq.com>
2023-02-03 09:08:08 +0800

    2023/02/03 Modify lwr-project-single-page-demo

--- __lwr_cache__/lwc_compiled_modules/cache/example_app_app_css_0_0_1.js
+++ __lwr_cache__/lwc_compiled_modules/cache/example_app_app_css_0_0_1.js
@@ -1,7 +1,7 @@
 function stylesheet(token, useActualHostSelector, useNativeDirPseudoclass) {
   var shadowSelector = token ? ("[" + token + "]") : "";
   var hostSelector = token ? ("[" + token + "-host]") : "";
-  return "main" + shadowSelector + " {margin: 30px;display: flex;flex-direction: column;align-items: center;}h1" + shadowSelector + " {color: #1798c1;}";
+  return "main" + shadowSelector + " {margin: 30px;display: flex;flex-direction: column;align-items: center;}h1" + shadowSelector + " {color: #1798c1;}.navigation-line-css" + shadowSelector + " {width: 100%;border: 1px solid white;border-radius: 5px;background-color: #52b9fb;}";
   /*LWC compiler v2.17.0*/
 }
 export default [stylesheet];
