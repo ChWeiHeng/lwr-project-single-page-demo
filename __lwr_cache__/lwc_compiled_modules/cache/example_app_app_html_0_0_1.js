d7794ac57403fbefad531b142d49dfa21967f300
chenweiheng <1164323899@qq.com>
2023-02-16 18:12:31 +0800

    2023/02/16

--- __lwr_cache__/lwc_compiled_modules/cache/example_app_app_html_0_0_1.js
+++ __lwr_cache__/lwc_compiled_modules/cache/example_app_app_html_0_0_1.js
@@ -5,13 +5,12 @@ import _implicitStylesheets from "./app.css";
 import _implicitScopedStylesheets from "./app.scoped.css?scoped=true";
 
 import _uiInput from "ui/input";
-import _recipeViewSource from "recipe/viewSource";
 import _recipeHelloForEach from "recipe/helloForEach";
 import _recipeApiSetterGetter from "recipe/apiSetterGetter";
 import _recipeApiProperty from "recipe/apiProperty";
 import _uiCard from "ui/card";
 import {parseFragment, registerTemplate} from "lwc";
-const $fragment1 = parseFragment`<main${3}><div class="navigation-line-css${0}"${2}>Test Navigation Bar!</div><div class="slds-box slds-p-around_none slds-m-top_x-small slds-m-bottom_medium slds-m-horizontal_none${0}"${2}></div></main>`;
+const $fragment1 = parseFragment`<main${3}><div class="navigation-line-css${0}"${2}>Test Navigation Bar!</div></main>`;
 const stc0 = {
   props: {
     "title": "Computer Software"
@@ -25,12 +24,6 @@ const stc2 = {
   key: 4
 };
 const stc3 = {
-  attrs: {
-    "slot": "footer"
-  },
-  props: {
-    "source": "recipe/helloBinding"
-  },
   key: 6
 };
 const stc4 = {
@@ -38,9 +31,6 @@ const stc4 = {
 };
 const stc5 = {
   key: 8
-};
-const stc6 = {
-  key: 9
 };
 function tmpl($api, $cmp, $slotset, $ctx) {
   const {st: api_static_fragment, d: api_dynamic_text, t: api_text, h: api_element, b: api_bind, c: api_custom_element} = $api;
@@ -54,7 +44,7 @@ function tmpl($api, $cmp, $slotset, $ctx) {
     on: {
       "change": _m0 || ($ctx._m0 = api_bind($cmp.handleChange))
     }
-  })]), api_custom_element("recipe-view-source", _recipeViewSource, stc3, [api_text("Change the value of a bound property when the value of an input field changes. Type something in the input field to see the recipe in action.")]), api_custom_element("recipe-hello-for-each", _recipeHelloForEach, stc4), api_custom_element("recipe-api-setter-getter", _recipeApiSetterGetter, stc5), api_custom_element("recipe-api-property", _recipeApiProperty, stc6)])];
+  })]), api_custom_element("recipe-hello-for-each", _recipeHelloForEach, stc3), api_custom_element("recipe-api-setter-getter", _recipeApiSetterGetter, stc4), api_custom_element("recipe-api-property", _recipeApiProperty, stc5)])];
   /*LWC compiler v2.17.0*/
 }
 export default registerTemplate(tmpl);
